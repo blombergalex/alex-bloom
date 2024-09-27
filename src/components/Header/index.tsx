fc2596b88c665aaf1e4b94afe4aedf02a03def14
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-09-27 10:50:47 +0200

    edit header color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div data-testid="header" className="flex flex-col">
-      <h2 className="text-right font-flowers text-2xl tracking-wider opacity-60 text-amber-500 pb-6 p-3 mr-2 md:text-4xl md:m-6 animate-fade-in-down">
+      <h2 className="text-right font-flowers text-2xl tracking-wider opacity-90 text-amber-500 pb-6 p-3 mr-2 md:text-4xl md:m-6 animate-fade-in-down">
         Welcome to my portfolio
       </h2>
       <div className="flex justify-between items-end font-SansNarrow md:tracking-wide">

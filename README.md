041f1fd0cc5ab86764a19b87b017cef66c3ea858
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-09-27 10:48:32 +0200

    add role

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 A portfolio project built with Next.js, React, Tailwind CSS. Unit and integration tests created with Jest and React Testing Library. 
-Visit at alex-bloom.vercel.app
+
+The purpose of this project was to learn the basis of creating unit and integration tests. 
+I wanter to create a platform in which I introduce myself and display my projects.
+The site consists of an introduction section where I tell about my story becoming a developer, following that is the projects section where the user is able to favourite any projects they like. Favourited projects are displayed in a Favourite projects list, where the user can click on the title to get to the site of the deployed project. Last but not least there is the Connect section, where my contact details are found. 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div data-testid="header" className="flex flex-col">
-      <h2 className="text-right font-flowers text-2xl tracking-wider opacity-60 text-amber-800 pb-6 p-3 mr-2 md:text-4xl md:m-6 animate-fade-in-down">
+      <h2 className="text-right font-flowers text-2xl tracking-wider opacity-60 text-amber-500 pb-6 p-3 mr-2 md:text-4xl md:m-6 animate-fade-in-down">
         Welcome to my portfolio
       </h2>
-      <div className="flex justify-between items-end">
-        <h1 className="self-end text-lg text-gray-200 uppercase font-SansNarrow ml-5 md:ml-10 sm:text-2xl md:tracking-wide">
-          ALEXANDRA BLOMBERG
-        </h1>
+      <div className="flex justify-between items-end font-SansNarrow md:tracking-wide">
+        <div className="text-gray-200 ml-5 md:ml-10 ">
+          <h1 className="self-end text-lg uppercase sm:text-2xl">
+            ALEXANDRA BLOMBERG
+          </h1>
+          <p className="capitalize opacity-80 md:text-lg">Frontend developer</p>
+        </div>
         <Image
           src="/images/floating-sunflowers.png"
           width={340}
